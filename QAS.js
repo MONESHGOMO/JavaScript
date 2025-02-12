4c747828bb50ea1a1dcb921c4cbbe8092a1070a5
monesh <moneshd16@gmail.com>
2025-02-12 20:24:22 +0530

    feat : Q/A challenge

---
+++ QAS.js
@@ -0,0 +1,31 @@
+
+// Question 1
+
+const price = 200;  // block scope and final keyword in java
+var product ="vivo"; // global scope variable like public
+let tax = 23;  // block scope like private in java 
+
+var vivo = product;
+
+
+console.log(product);
+var total = tax + price;
+console.log("Total price : " + total);
+console.log("Vivo : " + vivo);
+
+// Question 2
+
+var fruitName = "Apple";
+const fruitCount = 10;
+const fruitPrice = 20;
+
+var totalFruitPrice = fruitCount * fruitPrice;
+console.log("Total fruit price : " + totalFruitPrice);
+
+
+
+
+
+
+
+
