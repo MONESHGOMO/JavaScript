1748f90d560cbcd69e3907c479b07e8f229eb00d
monesh <moneshd16@gmail.com>
2025-02-12 20:53:00 +0530

    feat : Data Types in js

---
+++ DT.js
@@ -0,0 +1,36 @@
+var a = null;  // anything is  not defined in this a variable
+console.log(a);  // null
+
+/*
+
+resource : https://www.w3schools.com/js/js_datatypes.asp
+
+*/
+// number
+
+let height = 5;
+let width = 10;
+
+console.log("Area of a rectangle is: " + height * width);  
+
+// string
+
+var name = "John";
+var color = "red";
+
+console.log("name : " + name);
+
+// object
+
+const person = { firstName: "monesh", lastName: "gomo" };
+
+
+// Array object
+
+const cars = ["VOLVO", "BMW", "TOYOTA"];
+
+// date
+
+const date = new Date("12-02-2025");
+
+console.log(date);
