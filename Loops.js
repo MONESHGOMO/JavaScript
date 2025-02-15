c4847795754805bbde25ea1f3ef28b25f643b3ec
monesh <moneshd16@gmail.com>
2025-02-15 20:33:48 +0530

    feat : Loops

---
+++ Loops.js
@@ -0,0 +1,18 @@
+// for-loop
+
+for (let i = 0; i < 4; i++){
+    console.log("*");
+}
+
+// while-loop
+let count = 0;
+while (count > 3) {
+    console.log(count + 1);
+    count++;
+}
+
+// for-of loop (usefulfor arrays)
+const fruits = ["apple", "mango", "banana"];
+for (const fruit of fruits) {
+    console.log(fruit);
+}
