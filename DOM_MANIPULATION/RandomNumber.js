fd4473a778b2769e4ed53b4027e176e8aa386204
monesh <moneshd16@gmail.com>
2025-02-18 21:45:29 +0530

    feat : Random Number generator

---
+++ DOM_MANIPULATION/RandomNumber.js
@@ -0,0 +1,14 @@
+
+var a = Math.random();
+
+for (var i = 1; i <= 10; i++){
+    console.log(a * i);
+}
+
+
+var b = Math.floor(43.4) //   Argument must && something like float to int in java (7.34)-> (7)
+
+for (var j = 1; j < 5; j++){
+    console.log(b);
+}
+
