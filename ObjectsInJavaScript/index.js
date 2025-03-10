f4152474931efbef27c8ac4fcb5e2fc3b11bc1ff
monesh <moneshd16@gmail.com>
2025-03-10 19:17:21 +0530

    feat : instance in javascript

---
+++ ObjectsInJavaScript/index.js
@@ -0,0 +1,13 @@
+// instance in javascript
+
+/*
+  based on  ( KEY : VALUE  ) pair 
+  we need to use { } to create a object and the another way is using [ new Object(); ]
+
+*/
+let propertyOptions = { // from our project code
+    "buy": ["apartment", "villa", "penthouse", "land", "office_space", "warehouse", "retail_space", "building"],
+    "rent": ["apartment", "villa", "penthouse", "office_space", "warehouse", "retail_space", "labour_accommodation"]
+}; 
+// own code
+
